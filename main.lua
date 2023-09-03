3fc6f7458418b04edf9f211f4c420e3ec032f229
MagnunAVF <magnunavf@gmail.com>
2023-09-03 17:05:10 -0300

    Create the target

--- main.lua
+++ main.lua
@@ -1,8 +1,14 @@
 function love.load()
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
 end
 
 function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
 end
