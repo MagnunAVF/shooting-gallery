d3a5440cd6cc02bf729a0804ea1e2b46f427e14d
MagnunAVF <magnunavf@gmail.com>
2023-09-03 17:07:10 -0300

    Implement hit the target logic

--- main.lua
+++ main.lua
@@ -21,3 +21,16 @@ function love.draw()
     love.graphics.setFont(gameFont)
     love.graphics.print(score, 0, 0)
 end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    if button == 1 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+        end
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1)^2 + (y2-y1)^2)
+end
