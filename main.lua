fff6373598d682cbb6ee28bf53d5dde41bb307cc
MagnunAVF <magnunavf@gmail.com>
2023-09-03 17:05:52 -0300

    Add the score

--- main.lua
+++ main.lua
@@ -3,6 +3,11 @@ function love.load()
     target.x = 300
     target.y = 300
     target.radius = 50
+
+    score = 0
+    timer = 0
+
+    gameFont = love.graphics.newFont(40)
 end
 
 function love.update(dt)
@@ -11,4 +16,8 @@ end
 function love.draw()
     love.graphics.setColor(1, 0, 0)
     love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(gameFont)
+    love.graphics.print(score, 0, 0)
 end
